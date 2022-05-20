556e95d5c233039171c72b49af5caf36e04617b9
Oli0089 <40163619+Oli0089@users.noreply.github.com>
2022-05-20 15:28:04 +0100

    Add files via upload
    new hellow world file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package repository;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World3 is this in jenkins");
+	}
+
+}
