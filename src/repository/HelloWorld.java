aad75f7926a0292ee7a2b8aa110a49e5e4a7790a
Oliver Keeys <oliver.keeys@coveainsurance.co.uk>
2022-07-08 11:46:31 +0100

    changed to 7

--- src/repository/HelloWorld.java
+++ src/repository/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World6 is this in jenkins2. I am testing");
+		System.out.println("Hello World7 is this in jenkins2. I am testing");
 	}
 
 }
